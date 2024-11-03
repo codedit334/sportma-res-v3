24818df68c88eb98178e9d6c4596c34e6f1c8a8d
codedit334 <oussama7elabdioui@gmail.com>
2024-11-03 15:13:15 +0100

    Update router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,25 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: () => import("../views/Dashboard.vue"),
-    },
-    {
-      path: "/calender",
-      component: () => import("../views/Calender.vue"),
-    },
-    {
-      path: "/data",
-      component: () => import("../views/Datatable.vue"),
-    },
-    {
-      path: "/calender/configuration",
-      component: () => import("../views/Settings.vue"),
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            component: () => import("../views/Dashboard.vue"),
+        },
+        {
+            path: "/login",
+            component: () => import("../views/Auth/Login.vue"),
+        },
+        {
+            path: "/calender",
+            component: () => import("../views/Calender.vue"),
+        },
+        {
+            path: "/data",
+            component: () => import("../views/Datatable.vue"),
+        },
+        {
+            path: "/calender/configuration",
+            component: () => import("../views/Settings.vue"),
+        },
+    ],
 });
 
 export default router;
