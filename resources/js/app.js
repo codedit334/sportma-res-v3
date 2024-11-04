b2d3a6bf3879cc6430b891464d1bad9766a7eb4f
codedit334 <oussama7elabdioui@gmail.com>
2024-11-04 14:00:26 +0100

    Translate to french

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,31 @@
-import './bootstrap';
+import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store"; 
+import store from "./store";
 
 // Vuetify
 import "vuetify/styles"; // Ensure you are using css-loader
 import { createVuetify } from "vuetify";
-import '@mdi/font/css/materialdesignicons.css';
+import "@mdi/font/css/materialdesignicons.css";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { VTimePicker } from 'vuetify/labs/VTimePicker'
+import { VTimePicker } from "vuetify/labs/VTimePicker";
+// Translations provided by Vuetify
+import { pl, zhHans, fr } from "vuetify/locale";
 
 export default createVuetify({
-  components: {
-    VTimePicker,
-  },
-})
+    components: {
+        VTimePicker,
+    },
+});
 
 const vuetify = createVuetify({
-  components,
-  directives,
+    components,
+    directives,
+    locale: {
+        locale: "fr",
+        messages: { fr },
+    },
 });
 
 store.dispatch("auth/initializeAuth");
@@ -27,3 +33,4 @@ store.dispatch("auth/initializeAuth");
 import router from "./router";
 
 createApp(App).use(store).use(router).use(vuetify).mount("#app");
+
