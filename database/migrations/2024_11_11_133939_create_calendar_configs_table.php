81c9318e7152111c778c0dff2f7d6f805fc76432
codedit334 <oussama7elabdioui@gmail.com>
2024-11-11 14:40:27 +0100

    Calender configs table

---
+++ database/migrations/2024_11_11_133939_create_calendar_configs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarConfigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar_configs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id')->unique(); // Ensure only one config per company
+            $table->unsignedBigInteger('created_by_user_id');
+            $table->json('configurations');  // Store configurations as JSON
+            $table->timestamps();
+
+            // Foreign Key Constraints
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('created_by_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar_configs');
+    }
+}
