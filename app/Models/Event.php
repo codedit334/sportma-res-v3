645dde4da6df7e88ca393411302405c996addfab
codedit334 <oussama7elabdioui@gmail.com>
2024-11-11 14:47:04 +0100

    Reservations model

---
+++ app/Models/Event.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'created_by_user_id',
+        'reservations',
+    ];
+
+    protected $casts = [
+        'reservations' => 'array',  // Automatically cast JSON to array
+    ];
+
+    /**
+     * Get the company that owns the event.
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    /**
+     * Get the user who created the event.
+     */
+    public function createdByUser()
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
+}
